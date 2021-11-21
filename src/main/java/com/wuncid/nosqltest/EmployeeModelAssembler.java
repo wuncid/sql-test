c62a2f041c0d8669b167a0802e99e282bb244ac1
Silver Lumi <silverlumi@silvers-air.home>
2021-11-21 15:41:12 +0000

    first commit

---
+++ src/main/java/com/wuncid/nosqltest/EmployeeModelAssembler.java
@@ -0,0 +1,22 @@
+package com.wuncid.nosqltest;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
+
+import com.wuncid.nosqltest.controller.EmployeeController;
+import com.wuncid.nosqltest.model.Employee;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.RepresentationModelAssembler;
+import org.springframework.stereotype.Component;
+
+@Component
+public
+class EmployeeModelAssembler implements RepresentationModelAssembler<Employee, EntityModel<Employee>> {
+
+    @Override
+    public EntityModel<Employee> toModel(Employee employee) {
+
+        return EntityModel.of(employee, //
+                linkTo(methodOn(EmployeeController.class).one(employee.getId())).withSelfRel(),
+                linkTo(methodOn(EmployeeController.class).all()).withRel("employees"));
+    }
+}
